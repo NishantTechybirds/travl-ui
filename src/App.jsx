2e2bb9539ebfe100892aef93f5f0b48e8b1aa785
NishantTechybirds <nishant.chaudhary.pr.13@gmail.com>
2022-11-12 10:45:18 +0530

    initial commit, project setup with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { useState } from "react";
+import logo from "./logo.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500">Travl Project</div>;
+}
+
+export default App;
