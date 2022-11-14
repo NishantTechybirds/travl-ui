39426d00b9cec4fdc12eb16c10252798528c917b
NishantTechybirds <nishant.chaudhary.pr.13@gmail.com>
2022-11-14 18:00:30 +0530

    added bar chart on dashboard

--- src/pages/dashboardPage/index/Dashboard.jsx
+++ src/pages/dashboardPage/index/Dashboard.jsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
+import BarChart from "../../../components/BarChart";
 import SideBar from "../sidebar/SideBar";
 
 const Dashboard = () => {
   return (
-    <div className="">
+    <div className="flex">
       <SideBar></SideBar>
-      <div className="bg-yellow-500  grow">
-        <Outlet />
+      <div className="h-screen p-2 grow">
+        <div className="w-1/3 m-10 border border-gray-200 shadow-md h-2/5">
+          <BarChart></BarChart>
+        </div>
       </div>
     </div>
   );
